cd0ea2ed315e18a3332be6de199c9f78b48ff34e
james medina <jamesmedina7787@outlook.com>
2018-07-06 17:07:21 -0400

    new page working and heroku ready

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var ejs = require('ejs')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+
+  return res.render('new')
+})
+
+app.listen(PORT, ()=>{
+  console.log('listening on port 3000')
+})
